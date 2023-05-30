f732171d676f0f7305340ed6c31a6b92df107df6
RicardoAlvarezHernandez1 <rica07b@gmail.com>
2023-05-30 19:53:11 +0100

    feat(Personaje) : atributos y constructor

---
+++ src/net/salesianos/Personajes/Personaje.java
@@ -0,0 +1,16 @@
+package net.salesianos.Personajes;
+
+public class Personaje {
+    
+    private int salud;
+    private int defensa;
+    private int fuerza;
+   
+    public Personaje(int defensa, int fuerza) {
+        this.salud = 12;
+        this.defensa = defensa;
+        this.fuerza = fuerza;
+    }
+
+    
+}
