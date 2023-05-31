7cd48b0704cb444fd1ef31ac0f2265a9939370cb
RicardoAlvarezHernandez1 <rica07b@gmail.com>
2023-05-31 18:07:54 +0100

    feat(Mob) : getters y setters + atributo fuerza

--- src/net/salesianos/Mobs/Mob.java
+++ src/net/salesianos/Mobs/Mob.java
@@ -3,12 +3,27 @@ package net.salesianos.Mobs;
 
 public abstract class Mob {
     protected int salud;
+    protected int fuerza;
 
     public Mob(int salud) {
         this.salud = salud;
     }
 
+    public void setSalud(int salud) {
+        this.salud = salud;
+    }
+
+    public int getSalud() {
+        return salud;
+    }
+
     public abstract void moverse();
+
+    
+
+
+
+    
 
     
     
