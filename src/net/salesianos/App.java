effd009086687d5cb7ad3c1e7fba0dcc6d1f81e0
RicardoAlvarezHernandez1 <rica07b@gmail.com>
2023-05-31 13:32:36 +0100

    fix(app) : error de import y paquetes

---
+++ src/net/salesianos/App.java
@@ -0,0 +1,12 @@
+package net.salesianos;
+
+import net.salesianos.Personajes.*;
+import net.salesianos.Mobs.*;
+import net.salesianos.interfaces.*;
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+    }
+}
